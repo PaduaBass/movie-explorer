d0425d0d13708ede9e213ea78b49d646d42607e1
Padua <paduabass12345678@gmail.com>
2021-05-15 04:44:13 -0300

    include shimmer loading

--- src/pages/Player/index.tsx
+++ src/pages/Player/index.tsx
@@ -1,48 +1,62 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { ListRenderItem, Platform, FlatList, Dimensions, Text } from 'react-native';
-import { Video } from 'expo-av';
+import React, { useEffect, useState, createRef } from 'react';
+import { ListRenderItem, Platform, FlatList, Dimensions, Text, Animated, View } from 'react-native';
 import { useRoute } from '@react-navigation/core';
 import { DiscoverMovie } from '../../contexts/interfaces';
-import video from '../../../assets/video.mp4';
-import YoutubePlayer, { YoutubeIframeRef } from "react-native-youtube-iframe";
+import YoutubePlayer, { YoutubeIframeProps, YoutubeIframeRef } from "react-native-youtube-iframe";
 import api from '../../services/api';
 const window = Dimensions.get('window');
 
-import { Container } from './styles';
-
+import { Container, ContainerPlayer, ContainerShimmer, ShimmerPlaceholder, YouTubeWebView } from './styles';
+import LoadingImage from '../../components/LoadingImage';
 const Player: React.FC = () => {
-    const playerRef = useRef<YoutubeIframeRef>(null);
-
     const movie = useRoute().params as DiscoverMovie;
     const [clips, setClips] = useState<any>(null);
-    useEffect(() => {
-        async function getVideoMovie() {
-            if (movie.title) {
-                const response = await api.get(`/movie/${movie.id}/videos?api_key=b7b1762c97b44651d52bbe7e7fc52f09`)
-                setClips(response.data);
 
-            } else {
-                const response = await api.get(`/tv/${movie.id}/videos?api_key=b7b1762c97b44651d52bbe7e7fc52f09`)
-                setClips(response.data);
-            }
+
+    async function getVideoMovie() {
+        if (movie.title) {
+            const response = await api.get(`/movie/${movie.id}/videos?api_key=b7b1762c97b44651d52bbe7e7fc52f09`)
+            setClips(response.data);
+
+        } else {
+            const response = await api.get(`/tv/${movie.id}/videos?api_key=b7b1762c97b44651d52bbe7e7fc52f09`)
+            setClips(response.data);
         }
+    }
+
+
+    useEffect(() => {
         getVideoMovie();
+
     }, [])
 
+
     const renderItem: ListRenderItem<any> = ({ item: movie }) => {
-        if(Platform.OS === "web") {
-            return <Text>
-                <iframe width={window.width} style={{ borderRadius: 4, marginTop: 10 }} height="600" src={`https://www.youtube.com/embed/${movie.key}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-            </Text>
+        if (Platform.OS === "web") {
+            return <ContainerPlayer>
+                <ContainerShimmer>
+                    <ShimmerPlaceholder  height={530} />
+                </ContainerShimmer>
+                <Text>
+                    <iframe width={window.width} style={{ borderRadius: 4, marginTop: 10 }} height="600" src={`https://www.youtube.com/embed/${movie.key}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+                </Text>
+            </ContainerPlayer>
         }
-        return <>
-                
-                <YoutubePlayer  key={String(movie.id)} play={false} width={window.width} height={250}
-                    videoId={movie.key}
-                    webViewProps={{ renderToHardwareTextureAndroid: true, onTouchEnd: undefined, onTouchEndCapture: undefined  }}
-                    
-                />
-        </>
+        return <ContainerPlayer>
+            <ContainerShimmer>
+                <ShimmerPlaceholder  />
+            </ContainerShimmer>
+            <YouTubeWebView key={String(movie.id)} play={false}  height={250}
+                videoId={movie.key}
+                webViewProps={{ renderToHardwareTextureAndroid: true, onTouchEnd: undefined, onTouchEndCapture: undefined }}
+                onReady={() => { }}
+                webViewStyle={{ borderRadius: 4 }}
+                allowWebViewZoom={false}
+
+            />
+
+
+        </ContainerPlayer>
     }
 
     return <Container>
--- src/pages/Player/styles.ts
+++ src/pages/Player/styles.ts
@@ -1,7 +1,41 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
+import { createShimmerPlaceholder } from 'react-native-shimmer-placeholder';
+import { LinearGradient } from 'expo-linear-gradient';
+import YoutubePlayer, { YoutubeIframeProps, YoutubeIframeRef } from "react-native-youtube-iframe";
+
+const ShimmerCompoent = createShimmerPlaceholder(LinearGradient);
 
 export const Container = styled.View`
     flex: 1;
     background: #323232;
+    
+`;
 
+export const ContainerPlayer = styled.View`
+    width: 100%;
+    height: ${Platform.OS === "web" ? "604px" : "250px"};
+    margin-bottom: ${Platform.OS === "web" ? "30px" : "0px"};
+
+`;
+
+export const ContainerShimmer = styled.View`
+    width: 100%;
+    height: ${Platform.OS === "web" ? "600px" : "200px"};
+    position: absolute;
+    border-radius: 4px;
+    z-index: -1;
+
+`;
+
+export const ShimmerPlaceholder = styled(ShimmerCompoent)`
+    height: ${Platform.OS === "web" ? "600px" : "231px"};
+    width: 100%;
+    border-radius: 4px;
+    margin-top: ${Platform.OS === "web" ? "10px" : "0px"};
+
+`;
+
+export const YouTubeWebView = styled(YoutubePlayer)`
+    border-radius: 4px
 `;
